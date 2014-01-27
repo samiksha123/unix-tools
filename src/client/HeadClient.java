31d3868f349088eae84be6574b2d70190f8dfe4f
samiksha <samiksha@thoughtworks.com>
2014-01-27 09:57:46 +0530

    config file added

--- src/client/HeadClient.java
+++ src/client/HeadClient.java
@@ -3,22 +3,34 @@ package client;
 import samiksha.unixtools.Head;
 import samiksha.unixtools.readfile.ReadFile;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 public class HeadClient {
     public static void main(String[] args) throws IOException {
         int noOfLines;
-        String filename = args[0], value = "10";
-        String result;
-        Head head = new Head();
         ReadFile read = new ReadFile();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("-")) value = args[i].substring(1);
-            else filename = args[i];
+        String filename = args[0];
+        Head head = new Head();
+
+        try{
+            FileReader conFile = new FileReader("config.properties");
+            Properties pr = new Properties();
+            pr.load(conFile);
+            String value = pr.getProperty("headDefaultLines");
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].startsWith("-")) value = args[i].substring(1);
+                else filename = args[i];
+            }
+            noOfLines = Integer.parseInt(value);
+            System.out.println(noOfLines);
+        }catch(Exception e){
+            noOfLines = 10;
         }
-        noOfLines = Integer.parseInt(value);
+
         String input = read.readFile(filename);
-        result = head.printLines(input, noOfLines);
+        String result = head.printLines(input, noOfLines);
         System.out.println(result);
     }
 }
--- src/client/TailClient.java
+++ src/client/TailClient.java
@@ -3,20 +3,30 @@ package client;
 import samiksha.unixtools.readfile.ReadFile;
 import samiksha.unixtools.Tail;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 public class TailClient {
     public static void main(String[] args) throws IOException {
         int noOfLines;
-        String res;
-        String filename = args[0], value = "10";
+        String res, value;
+        String filename = args[0];
         Tail tail = new Tail();
         ReadFile read = new ReadFile();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("-")) value = args[i].substring(1);
-            else filename = args[i];
+        try {
+            FileReader conFile = new FileReader("config.properties");
+            Properties pr = new Properties();
+            pr.load(conFile);
+            value = pr.getProperty("tailDefaultLines");
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].startsWith("-")) value = args[i].substring(1);
+                else filename = args[i];
+            }
+            noOfLines = Integer.parseInt(value);
+        } catch (Exception e) {
+            noOfLines = 10;
         }
-        noOfLines = Integer.parseInt(value);
         String input = read.readFile(filename);
         res = tail.printLines(input, noOfLines);
         System.out.println(res);
